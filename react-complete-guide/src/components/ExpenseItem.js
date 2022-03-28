705f83a63fb77adda4b3c82999372878ce8e49bf
OxygenJam <triztandriz@gmail.com>
2022-03-29 01:46:32 +0800

    added expensedate component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 // A component in react is just a function
 
 // Only 1 root element in JSX
-
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const expenseDate = props.date;
-    const expenseTitle = props.title;
-    const expenseAmount = props.amount;
+	const expenseTitle = props.title;
+	const expenseAmount = props.amount;
 
 	return (
 		<div className="expense-item">
-			<div>{expenseDate.toLocaleDateString()}</div>
+            <ExpenseDate date={props.date}/>
 			<div className="expense-item__description">
 				<h2>{expenseTitle}</h2>
 				<div className="expense-item__price">${expenseAmount}</div>
