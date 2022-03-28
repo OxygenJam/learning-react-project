804bac52cb26f0cfad39db27e3bdac80f2ef790f
OxygenJam <triztandriz@gmail.com>
2022-03-29 01:31:23 +0800

    working with props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,8 +1,23 @@
 // A component in react is just a function
 
-function ExpenseItem() {
-    return <h2>Expense Item!</h2>
-}
+// Only 1 root element in JSX
 
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const expenseDate = props.date;
+    const expenseTitle = props.title;
+    const expenseAmount = props.amount;
+
+	return (
+		<div className="expense-item">
+			<div>{expenseDate.toLocaleDateString()}</div>
+			<div className="expense-item__description">
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price">${expenseAmount}</div>
+			</div>
+		</div>
+	);
+}
 
 export default ExpenseItem;
